76b506508e7d26fd51003eaec0c5baa906d5173c
Mauricio Leon <194124-0@iberoleon.edu.mx>
2025-09-04 13:23:56 -0600

    Subir

---
+++ clases/avanzadas.py
@@ -0,0 +1,20 @@
+class Operaciones:
+    def __init__(self):
+        self.base = 0
+        self.exponente = 0
+        self.resultado = 0
+        
+    def leerNumeros(self):
+        while True:
+            try:
+                self.base = float(input("Número base: "))
+                self.exponente = float(input("Número exponente: "))
+                break
+            except ValueError:
+                print("Número inválido. Intenta de nuevo.")
+    
+    def potencia(self):
+        self.resultado = self.base ** self.exponente
+    
+    def mostrarResultado(self):   # 👈 este método es el que te faltaba
+        print(f"{self.base} elevado a {self.exponente} es igual a {self.resultado}")
---
+++ main.py
@@ -0,0 +1,10 @@
+from clases.avanzadas import Operaciones
+
+def main():
+    op = Operaciones()
+    op.leerNumeros()
+    op.potencia()
+    op.mostrarResultado()
+    
+if __name__ == "__main__":
+    main()
